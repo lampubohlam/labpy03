0f9d066161c6f96c7a2a46d47ed1886a5b512a4b
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-11-07 13:07:42 +0700

    Add files via upload

---
+++ modul praktikum2&3.py/lat.py
@@ -0,0 +1,13 @@
+print("_____Abdul aziz____")
+a, b, c =(
+    int(input("masukan nilai a: " )),
+    int(input("masukan nilai b: ")),
+    int(input("masukan nilai c: "))
+)
+if a>b and a<c:
+    print("A yang terbesar")
+elif b>a and b>c:
+    print("B yang terbesar")
+else:
+    print("C yang terbesar")
+
