6dcfb0ebbc2388d9d20ba726e3d0f9adf7c0394a
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-11-07 13:08:05 +0700

    Add files via upload

---
+++ perulangan.py/lat1.py
@@ -0,0 +1,9 @@
+print("____abdul aziz______")
+from random import random
+n = int(input ("masukan beberapa perulangan"))
+while n==n: 
+    break
+for i in range(n):
+    bil = random( )%0.5
+    print('perulangan ke : ',bil)
+
---
+++ perulangan.py/lat2perulangan.py
@@ -0,0 +1,6 @@
+print("_________Abdul aziz_____")
+for i in range(0,10):
+    print()
+    print(i, end="\t")
+    for j in range(1,10):
+        print(i+j,end="\t")
