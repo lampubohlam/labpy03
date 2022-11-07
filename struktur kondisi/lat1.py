ccac71391142cffaa7a46199eb82066f876e27bc
lampubohlam <116137169+lampubohlam@users.noreply.github.com>
2022-11-07 13:09:00 +0700

    Add files via upload

---
+++ struktur kondisi/lat1.py
@@ -0,0 +1,9 @@
+print('Menentukan bilangan terbesar dari 2 bilangan')
+a = int(input("Masukkan nilai A:"))
+b = int(input("Masukkan nilai B:"))
+if a > b:
+    print("A =", a, "yang terbesar")
+elif b > a:
+    print("B=", b, "yang terbesar")    
+    
+    
